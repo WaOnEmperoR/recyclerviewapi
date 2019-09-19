983de77c6ef90f3d28510f553af304428390c3cb
WaOnEmperoR <themusketeer17@gmail.com>
2019-09-19 09:35:56 +0700

    cleaning

--- app/src/main/java/id/govca/recyclerviewapi/widget/StackRemoteViewsFactory.java
+++ app/src/main/java/id/govca/recyclerviewapi/widget/StackRemoteViewsFactory.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+
 import android.net.Uri;
 import android.os.Binder;
 import android.os.Bundle;
@@ -16,24 +16,14 @@ import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
 import id.govca.recyclerviewapi.R;
-import id.govca.recyclerviewapi.entity.Favorite;
 import id.govca.recyclerviewapi.helper.Constants;
-import id.govca.recyclerviewapi.helper.DatabaseClient;
-import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.observers.DisposableObserver;
-import io.reactivex.schedulers.Schedulers;
 
 public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory{
-    private final List<Bitmap> mWidgetItems = new ArrayList<>();
     private List<String> listFavoriteMovies = new ArrayList<>();
 
-    private CompositeDisposable disposable = new CompositeDisposable();
     private final Context mContext;
 
     public static final Uri CONTENT_URI = new Uri.Builder().scheme("content")
@@ -131,52 +121,4 @@ public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFa
         return false;
     }
 
-    private Observable<List<Favorite>> getFavorites() {
-        Observable<List<Favorite>> observable = Observable.fromCallable(new Callable<List<Favorite>>() {
-            @Override
-            public List<Favorite> call() {
-                return DatabaseClient.getInstance(mContext)
-                        .getAppDatabase()
-                        .getFavoriteDAO()
-                        .fetchFavoriteAll();
-            }
-        });
-
-        return observable;
-    }
-
-    private void getFavoritesObservable(){
-        Observable<List<Favorite>> favoriteObservable = getFavorites();
-
-        disposable.add(
-                favoriteObservable
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(new DisposableObserver<List<Favorite>>() {
-                            @Override
-                            public void onNext(List<Favorite> favorites) {
-                                for(int i=0; i<favorites.size(); i++)
-                                {
-                                    String path = favorites.get(i).getPoster_path().substring(1);
-                                    String full_path = Constants.IMAGE_ROOT_SMALL + path;
-
-                                    Log.d(TAG, full_path);
-                                    listFavoriteMovies.add(full_path);
-                                }
-                                Log.d(TAG, String.valueOf(listFavoriteMovies.size()));
-                            }
-
-                            @Override
-                            public void onError(Throwable e) {
-                                Log.e(TAG, e.getMessage());
-                            }
-
-                            @Override
-                            public void onComplete() {
-                                Log.d(TAG, "onComplete from RXJava");
-                                this.dispose();
-                            }
-                        })
-        );
-    }
 }
