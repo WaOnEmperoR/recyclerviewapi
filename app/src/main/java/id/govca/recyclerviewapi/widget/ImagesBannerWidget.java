7e6e6aa67effd8511a42bcdcc61cebd424d30fb8
WaOnEmperoR <themusketeer17@gmail.com>
2019-09-19 09:08:49 +0700

    checkpoint Stack View using Glide

--- app/src/main/java/id/govca/recyclerviewapi/widget/ImagesBannerWidget.java
+++ app/src/main/java/id/govca/recyclerviewapi/widget/ImagesBannerWidget.java
@@ -11,38 +11,32 @@ import android.widget.Toast;
 
 import id.govca.recyclerviewapi.R;
 
-/**
- * Implementation of App Widget functionality.
- */
-public class MovieStackWidget extends AppWidgetProvider {
+public class ImagesBannerWidget extends AppWidgetProvider {
+    private static final String TOAST_ACTION = "id.govca.recyclerviewapi.TOAST_ACTION";
+    public static final String EXTRA_ITEM = "id.govca.recyclerviewapi.EXTRA_ITEM";
 
-    private static final String TOAST_ACTION = "id.govca.TOAST_ACTION";
-    public static final String EXTRA_ITEM = "id.govca.EXTRA_ITEM";
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
+    private static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
         Intent intent = new Intent(context, StackWidgetService.class);
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
         intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.movie_stack_widget);
         views.setRemoteAdapter(R.id.stack_view, intent);
         views.setEmptyView(R.id.stack_view, R.id.empty_view);
 
-        Intent toastIntent = new Intent(context, MovieStackWidget.class);
-        toastIntent.setAction(MovieStackWidget.TOAST_ACTION);
+        Intent toastIntent = new Intent(context, ImagesBannerWidget.class);
+        toastIntent.setAction(ImagesBannerWidget.TOAST_ACTION);
         toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
         intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-        PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 50, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         views.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent);
 
-        // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
+    /*
+    Update widget
+     */
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
@@ -51,16 +45,9 @@ public class MovieStackWidget extends AppWidgetProvider {
         }
     }
 
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
-
+    /*
+    Gunakan onReceive untuk menerima broadcast
+     */
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
@@ -72,4 +59,3 @@ public class MovieStackWidget extends AppWidgetProvider {
         }
     }
 }
-
--- app/src/main/java/id/govca/recyclerviewapi/widget/StackRemoteViewsFactory.java
+++ app/src/main/java/id/govca/recyclerviewapi/widget/StackRemoteViewsFactory.java
@@ -4,30 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ImageView;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
-import androidx.lifecycle.MutableLiveData;
-
 import com.bumptech.glide.Glide;
-import com.pranavpandey.android.dynamic.toasts.DynamicToast;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
-import id.govca.recyclerviewapi.GlobalApplication;
 import id.govca.recyclerviewapi.R;
 import id.govca.recyclerviewapi.entity.Favorite;
 import id.govca.recyclerviewapi.helper.Constants;
@@ -38,15 +26,14 @@ import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
-
-public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory {
-    private final Context mContext;
-    private CompositeDisposable disposable = new CompositeDisposable();
-    private final String TAG = this.getClass().getSimpleName();
-
-    private Context context = GlobalApplication.getAppContext();
-
+public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory{
+    private final List<Bitmap> mWidgetItems = new ArrayList<>();
     private List<String> listFavoriteMovies = new ArrayList<>();
+
+    private CompositeDisposable disposable = new CompositeDisposable();
+    private final Context mContext;
+
+    private final String TAG = this.getClass().getSimpleName();
 
     StackRemoteViewsFactory(Context context) {
         mContext = context;
@@ -59,7 +46,36 @@ public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFa
 
     @Override
     public void onDataSetChanged() {
-        getFavoritesObservable();
+//        getFavoritesObservable();
+
+        Bitmap theBitmap = null;
+        try {
+            theBitmap = Glide.
+                    with(mContext).
+                    asBitmap().
+                    load("https://image.tmdb.org/t/p/w150_and_h225_bestv2/eeHI5iBSCOxj4HGSOmFM6azBmwb.jpg").
+                    into(150, 225). // Width and height
+                    get();
+        } catch (ExecutionException e) {
+            Log.e(TAG, e.getMessage());
+        } catch (InterruptedException e) {
+            Log.e(TAG, e.getMessage());
+        }
+        mWidgetItems.add(theBitmap);
+
+        try {
+            theBitmap = Glide.
+                    with(mContext).
+                    asBitmap().
+                    load("https://image.tmdb.org/t/p/w150_and_h225_bestv2/lZMb3R3e5vqukPbeDMeyYGf2ZNG.jpg").
+                    into(150, 225). // Width and height
+                    get();
+        } catch (ExecutionException e) {
+            Log.e(TAG, e.getMessage());
+        } catch (InterruptedException e) {
+            Log.e(TAG, e.getMessage());
+        }
+        mWidgetItems.add(theBitmap);
     }
 
     @Override
@@ -69,17 +85,18 @@ public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFa
 
     @Override
     public int getCount() {
-        return listFavoriteMovies.size();
+        return mWidgetItems.size();
+//        return listFavoriteMovies.size();
     }
 
     @Override
     public RemoteViews getViewAt(int position) {
         RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_item);
-
+        Log.d(TAG, "enter " + position);
 //        Bitmap theBitmap = null;
 //        try {
 //            theBitmap = Glide.
-//                    with(context).
+//                    with(mContext).
 //                    asBitmap().
 //                    load(listFavoriteMovies.get(position)).
 //                    into(150, 225). // Width and height
@@ -89,16 +106,14 @@ public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFa
 //        } catch (InterruptedException e) {
 //            Log.e(TAG, e.getMessage());
 //        }
+//        rv.setImageViewBitmap(R.id.imageView, theBitmap);
+        rv.setImageViewBitmap(R.id.imageView, mWidgetItems.get(position));
 
-        Log.d(TAG, listFavoriteMovies.get(position));
-        Bitmap bitmap = callHttpConnection(listFavoriteMovies.get(position));
-
-//        rv.setImageViewBitmap(R.id.imageView, mWidgetItems.get(position));
-        rv.setImageViewBitmap(R.id.imageView, bitmap);
         Bundle extras = new Bundle();
-        extras.putInt(MovieStackWidget.EXTRA_ITEM, position);
+        extras.putInt(ImagesBannerWidget.EXTRA_ITEM, position);
         Intent fillInIntent = new Intent();
         fillInIntent.putExtras(extras);
+
         rv.setOnClickFillInIntent(R.id.imageView, fillInIntent);
         return rv;
     }
@@ -123,34 +138,11 @@ public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFa
         return false;
     }
 
-    private Bitmap callHttpConnection(String url) {
-        URL ulrn = null;
-        Bitmap bmp = null;
-
-        HttpURLConnection con = null;
-        try {
-            ulrn = new URL(url);
-            con = (HttpURLConnection) ulrn.openConnection();
-            InputStream is = con.getInputStream();
-            bmp = BitmapFactory.decodeStream(is);
-
-        } catch (MalformedURLException e) {
-            Log.e(TAG, e.getMessage());
-        } catch (IOException e) {
-            Log.e(TAG, e.getMessage());
-        } finally {
-            if (con != null)
-                con.disconnect();
-        }
-
-        return bmp;
-    }
-
     private Observable<List<Favorite>> getFavorites() {
         Observable<List<Favorite>> observable = Observable.fromCallable(new Callable<List<Favorite>>() {
             @Override
             public List<Favorite> call() {
-                return DatabaseClient.getInstance(context)
+                return DatabaseClient.getInstance(mContext)
                         .getAppDatabase()
                         .getFavoriteDAO()
                         .fetchFavoriteAll();
@@ -165,33 +157,33 @@ public class StackRemoteViewsFactory implements RemoteViewsService.RemoteViewsFa
 
         disposable.add(
                 favoriteObservable
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeWith(new DisposableObserver<List<Favorite>>() {
-                        @Override
-                        public void onNext(List<Favorite> favorites) {
-                            for(int i=0; i<favorites.size(); i++)
-                            {
-                                String path = favorites.get(i).getPoster_path().substring(1);
-                                String full_path = Constants.IMAGE_ROOT_SMALL + path;
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(new DisposableObserver<List<Favorite>>() {
+                            @Override
+                            public void onNext(List<Favorite> favorites) {
+                                for(int i=0; i<favorites.size(); i++)
+                                {
+                                    String path = favorites.get(i).getPoster_path().substring(1);
+                                    String full_path = Constants.IMAGE_ROOT_SMALL + path;
 
-                                listFavoriteMovies.add(full_path);
+                                    Log.d(TAG, full_path);
+                                    listFavoriteMovies.add(full_path);
+                                }
+                                Log.d(TAG, String.valueOf(listFavoriteMovies.size()));
                             }
-                            Log.d(TAG, String.valueOf(listFavoriteMovies.size()));
-                        }
 
-                        @Override
-                        public void onError(Throwable e) {
-                            Log.e(TAG, e.getMessage());
-                        }
+                            @Override
+                            public void onError(Throwable e) {
+                                Log.e(TAG, e.getMessage());
+                            }
 
-                        @Override
-                        public void onComplete() {
-                            Log.d(TAG, "onComplete from RXJava");
-                            this.dispose();
-                        }
-                    })
+                            @Override
+                            public void onComplete() {
+                                Log.d(TAG, "onComplete from RXJava");
+                                this.dispose();
+                            }
+                        })
         );
     }
-
 }
